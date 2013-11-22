f45814d61d8bb812f229e88c58b5656be2e535ee
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-22 19:40:01 +0100

    remove SFTP support
    now an add-on

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -126,9 +126,6 @@
 #ifdef HAS_FILESYSTEM_AFP
 #include "filesystem/AFPFile.h"
 #endif
-#ifdef HAS_FILESYSTEM_SFTP
-#include "filesystem/SFTPFile.h"
-#endif
 #include "addons/VFSEntry.h"
 #include "PartyModeManager.h"
 #ifdef HAS_VIDEO_PLAYBACK
@@ -2902,9 +2899,6 @@ void CApplication::Stop(int exitCode)
     g_RarManager.ClearCache(true);
 #endif
 
-#ifdef HAS_FILESYSTEM_SFTP
-    CSFTPSessionManager::DisconnectAllSessions();
-#endif
     CVFSEntryManager::Get().DisconnectAll();
 
 #if defined(TARGET_POSIX) && defined(HAS_FILESYSTEM_SMB)
@@ -4572,9 +4566,6 @@ void CApplication::ProcessSlow()
   gAfpConnection.CheckIfIdle();
 #endif
 
-#ifdef HAS_FILESYSTEM_SFTP
-  CSFTPSessionManager::ClearOutIdleSessions();
-#endif
   CVFSEntryManager::Get().ClearOutIdle();
 
   g_mediaManager.ProcessEvents();
@@ -5248,9 +5239,6 @@ void CApplication::CloseNetworkShares()
   gAfpConnection.Deinit();
 #endif
   
-#ifdef HAS_FILESYSTEM_SFTP
-  CSFTPSessionManager::DisconnectAllSessions();
-#endif
   CVFSEntryManager::Get().DisconnectAll();
 }
 
